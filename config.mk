557e2256723e26861363ef312dacf251fe06767d
elpaablo <donopwhile1@gmail.com>
2022-08-13 07:23:35 +0100

    monet: add option to apply tint overlay [3/3]
    * Bring back black theme overlay
    * Vivid colors -> Tint, as an extension of black theme.

--- config.mk
+++ config.mk
@@ -210,7 +210,8 @@ PRODUCT_PACKAGES += \
 
 # Themes
 PRODUCT_PACKAGES += \
-    AndroidVividThemeOverlay \
+    AndroidBlackThemeOverlay \
+    AndroidTintThemeOverlay \
     riceDroidThemesStub
 
 # Include {Lato,Rubik} fonts
